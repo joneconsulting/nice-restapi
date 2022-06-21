e949d1a7549096727ada9dace63f0d4b3a8d34cf
Dowon Lee <edowon0623@gmail.com>
2022-06-21 09:54:01 +0900

    upload a create function

---
+++ my-restful-service/src/main/java/com/example/myrestfulservice/contoller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.myrestfulservice.contoller;
+
+import com.example.myrestfulservice.bean.User;
+import com.example.myrestfulservice.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserDaoService service;
+
+    @Autowired
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET http://localhost:8080/users/10000
+    @GetMapping("/{id}")
+    public User retrieveUserById(@PathVariable(value = "id") int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping("")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+    }
+}
