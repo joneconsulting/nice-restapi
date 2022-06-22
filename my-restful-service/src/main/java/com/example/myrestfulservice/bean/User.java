985857a92bd0e9066c49a2ec2f713b2900f3aa67
edowon0623@gmail.com <Dwlee$09>
2022-06-22 11:49:25 +0900

    add an actuator lib

--- my-restful-service/src/main/java/com/example/myrestfulservice/bean/User.java
+++ my-restful-service/src/main/java/com/example/myrestfulservice/bean/User.java
@@ -10,6 +10,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -21,8 +24,12 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password", "ssn"})
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
